1d87c5a3dd9fb85b682739e28f1b8a0c3b282799
Vikash Kumar <itsmevikash09@gmail.com>
2021-05-16 23:24:46 +0530

    word fragment layout and class created

---
+++ app/src/main/java/com/aiden/wordapp/WordListFragment.kt
@@ -0,0 +1,13 @@
+package com.aiden.wordapp
+
+import androidx.fragment.app.Fragment
+import com.aiden.wordapp.databinding.FragmentWordLayoutBinding
+
+class WordListFragment : Fragment() {
+
+    private var _binding: FragmentWordLayoutBinding? = null
+    private val binding get() = _binding!!
+
+
+
+}
