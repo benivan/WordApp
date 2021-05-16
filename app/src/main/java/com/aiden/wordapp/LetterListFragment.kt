50d09ae37854f61d9f62d78e09c19c28919a77f5
Vikash Kumar <itsmevikash09@gmail.com>
2021-05-16 23:34:21 +0530

    letter fragment layout and class created

---
+++ app/src/main/java/com/aiden/wordapp/LetterListFragment.kt
@@ -0,0 +1,10 @@
+package com.aiden.wordapp
+
+import androidx.fragment.app.Fragment
+import com.aiden.wordapp.databinding.FragmentWordLayoutBinding
+
+class LetterListFragment : Fragment() {
+
+    private var _binding: FragmentWordLayoutBinding? = null
+    private val binding get() = _binding!!
+}
