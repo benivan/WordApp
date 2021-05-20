88bd9201163955e57dc77ad09a09fd4a50d9dfbc
Vikash Kumar <itsmevikash09@gmail.com>
2021-05-20 13:36:50 +0530

    new branch created

--- app/src/main/java/com/aiden/wordapp/LetterAdapter.kt
+++ app/src/main/java/com/aiden/wordapp/LetterAdapter.kt
@@ -1,4 +1,39 @@
 package com.aiden.wordapp
 
-class LetterAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.aiden.wordapp.databinding.ItemViewBinding
+
+class LetterAdapter : RecyclerView.Adapter<LetterAdapter.LetterViewHolder>() {
+
+
+    private val list = ('A').rangeTo('Z').toList()
+
+
+    inner class LetterViewHolder(private val itemBinding: ItemViewBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
+
+            fun bind(letter:Char) {
+                itemBinding.buttonItem.text =letter.toString()
+            }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LetterViewHolder {
+        val itemBinding =
+            ItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return LetterViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
+        val currentLetter = list[position]
+        holder.bind(currentLetter)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+
 }
